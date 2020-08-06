e93ac8e7871220d8a43daa8357ae0f76854416d0
Samuel Febreiro <samuelfebreiro@gmail.com>
2020-08-06 10:33:26 +0200

    Add files via upload

---
+++ n_total_sum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+	int n, sum = 0;
+	
+	cout<<"Introduce N: "; cin>>n;
+	
+	for(int 11010i=1;i<=n;i++){
+		sum += i;
+	}
+	
+	cout<<"The total sum is: "<<sum;	
+	
+	getch();
+	return 0;
+}
